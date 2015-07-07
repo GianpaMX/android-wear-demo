06085c37ac111b2038e95480a2387bf803dd6f97
Juan Villa <juan64@gmail.com>
2015-07-07 10:19:07 -0500

    Creating Conversation Activity

---
+++ app/src/main/java/mx/softux/androidweardemo/ConversationActivity.java
@@ -0,0 +1,19 @@
+package mx.softux.androidweardemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by juan on 7/7/15.
+ */
+public class ConversationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
