2a62a69b598b6135f2d7a84026af5ce91874ee12
Juan Villa <juan64@gmail.com>
2015-07-07 20:29:47 -0500

    Setting a public API Server

--- app/src/main/java/mx/softux/androidweardemo/Api.java
+++ app/src/main/java/mx/softux/androidweardemo/Api.java
@@ -19,7 +19,7 @@ public class Api {
                     .create();
 
             RestAdapter restAdapter = new RestAdapter.Builder()
-                    .setEndpoint("http://192.168.1.63:1337")
+                    .setEndpoint("http://softux.ddns.net:1337")
                     .setConverter(new GsonConverter(gson))
                     .build();
 
